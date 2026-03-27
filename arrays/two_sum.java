b217d4c96cf26332ca79cfd3ed038d711941c2cb
rscoder999 <rscodes999@gmail.com>
2026-03-27 16:18:24 +0530

    Create two_sum.java

---
+++ arrays/two_sum.java
@@ -0,0 +1,18 @@
+//Problem:Two sum
+//https://leetcode.com/problems/two-sum/description/
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int ans[]=new int[2];
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<=nums.length-1;j++){
+                if(nums[i]+nums[j]==target){
+                    ans[0]=i;
+                    ans[1]=j;
+                }
+            }
+        }
+        return ans;
+        
+    }
+}
